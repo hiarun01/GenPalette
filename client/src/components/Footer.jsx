f7714a95d5b3644f114bf8cfe93a99617ac57401
hiarun01 <hiarun02@gmail.com>
2025-07-07 23:54:52 +0530

    add header hero section and footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-white  py-6  text-center text-gray-600 text-sm">
+      <div className="container mx-auto px-4 flex flex-col md:flex-row justify-center items-center gap-2">
+        <div className="flex justify-center items-center">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-black transition"
+          >
+            Build by : arun & jay
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
