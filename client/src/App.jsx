7d14676ee38076f4e65c88f4ccffaf153af6e468
hiarun01 <hiarun02@gmail.com>
2025-07-07 23:08:16 +0530

    setup routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,27 @@
-import {Button} from "@/components/ui/button";
-function App() {
-  return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>GenPalette</Button>
-    </div>
-  );
-}
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Home from "./components/pages/Home";
+import Dashboard from "./components/pages/Dashboard/Dashboard";
+import AppLayout from "./AppLayout/AppLayout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+    ],
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
