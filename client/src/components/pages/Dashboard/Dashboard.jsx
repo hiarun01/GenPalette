c831aa96f438328602159569dbbeaba77f945754
hiarun01 <hiarun02@gmail.com>
2025-07-08 01:44:54 +0530

    handle input

--- client/src/components/pages/Dashboard/Dashboard.jsx
+++ client/src/components/pages/Dashboard/Dashboard.jsx
@@ -1,16 +1,21 @@
+import {useState} from "react";
 import {Button} from "../../ui/button";
 const Dashboard = () => {
+  const [userPrompt, setUserPrompt] = useState("");
+
   return (
-    <div className="mx-auto max-w-3xl h-auto py-20 ">
+    <div className="mx-auto max-w-3xl h-auto py-20 flex justify-center flex-col">
       {/* input Area */}
-      <div className="mb-10 ">
+      <div className="mb-10 mx-5">
         <textarea
-          className="border w-full px-2 pt-1 rounded-md font-mono"
+          value={userPrompt}
+          onChange={(e) => setUserPrompt(e.target.value)}
+          className="border w-full px-2 pt-1 rounded-md font-mono "
           placeholder="Tell us the mood, style, or theme you'd like your color palette to capture."
           rows={10}
         />
       </div>
-      <div>
+      <div className="mx-5">
         <Button className="w-full h-12 text-lg">Generate</Button>
       </div>
     </div>
